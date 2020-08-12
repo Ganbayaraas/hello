d90428faf3c6e36da2618d1195b68d93d200d84d
chimde <chimdeecj@gmail.com>
2020-08-12 13:25:53 +0800

    belneer nad qr

--- www/views/car/car-info.js
+++ www/views/car/car-info.js
@@ -1,1 +1,21 @@
-angular.module("carinfo.Ctrl", []).controller("carinfoCtrl", function ($ionicSlideBoxDelegate, $scope) {});
+angular.module("carinfo.Ctrl", []).controller("carinfoCtrl", function ($ionicSlideBoxDelegate, $scope) {
+  $scope.payOn = function () {
+    document.getElementById("paypopup").style.display = "block";
+  };
+  $scope.payOff = function () {
+    document.getElementById("paypopup").style.display = "none";
+  };
+  $scope.tabhide = function () {
+    document.getElementById("home-tab").style.display = "none";
+  };
+  $scope.shouldHide = function () {
+    switch ($state.current.name) {
+      case "statename1":
+        return true;
+      case "statename2":
+        return true;
+      default:
+        return false;
+    }
+  };
+});
