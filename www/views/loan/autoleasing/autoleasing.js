58949ec8c32f96eed2dbb241c0cebcfd0f2992bc
EnkhturBaina <eegii.111.gg@gmail.com>
2020-08-13 14:06:22 +0800

    keyboard fix

--- www/views/loan/autoleasing/autoleasing.js
+++ www/views/loan/autoleasing/autoleasing.js
@@ -1,18 +1,23 @@
 ﻿angular.module("autoleasing.Ctrl", []).controller("autoleasingCtrl", function ($scope, $ionicPlatform) {
   var input = document.getElementById("inputId");
   if (input) {
-    console.log("1", input);
+    function addThousandsSeparator(x) {
+      retVal = x ? parseFloat(x.replace(/,/g, "")) : 0;
+      return retVal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
     $scope.clearD = function () {
       input.value = input.value.slice(0, input.value.length - 1);
     };
+
     $scope.addCode = function (key) {
-      input.value = input.value + key;
+      input.value = addThousandsSeparator(input.value + key);
     };
 
     $scope.emptyCode = function () {
       input.value = "";
     };
+
     $scope.emptyCode();
   }
 });
