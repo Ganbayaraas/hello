61f135daf715ec4f2626fc6dfa24eab41fcc36ff
EnkhturBaina <eegii.111.gg@gmail.com>
2020-08-13 11:48:50 +0800

    custom keyboard

--- www/views/loan/autoleasing/autoleasing.js
+++ www/views/loan/autoleasing/autoleasing.js
@@ -1,3 +1,15 @@
 ﻿angular.module("autoleasing.Ctrl", []).controller("autoleasingCtrl", function ($scope, $ionicPlatform) {
   console.log("1");
+  var input = document.getElementById("inputId");
+  $scope.clearD = function () {
+    input.value = input.value.slice(0, input.value.length - 1);
+  };
+  $scope.addCode = function (key) {
+    input.value = input.value + key;
+  };
+
+  $scope.emptyCode = function () {
+    input.value = "";
+  };
+  $scope.emptyCode();
 });
