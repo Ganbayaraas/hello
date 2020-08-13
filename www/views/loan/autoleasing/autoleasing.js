dfe0f8565312c7e6198c4598362eb6cfe28d4360
EnkhturBaina <eegii.111.gg@gmail.com>
2020-08-13 11:51:25 +0800

    fix

--- www/views/loan/autoleasing/autoleasing.js
+++ www/views/loan/autoleasing/autoleasing.js
@@ -1,15 +1,18 @@
 ﻿angular.module("autoleasing.Ctrl", []).controller("autoleasingCtrl", function ($scope, $ionicPlatform) {
-  console.log("1");
   var input = document.getElementById("inputId");
-  $scope.clearD = function () {
-    input.value = input.value.slice(0, input.value.length - 1);
-  };
-  $scope.addCode = function (key) {
-    input.value = input.value + key;
-  };
+  if (input) {
+    console.log("1", input);
 
-  $scope.emptyCode = function () {
-    input.value = "";
-  };
-  $scope.emptyCode();
+    $scope.clearD = function () {
+      input.value = input.value.slice(0, input.value.length - 1);
+    };
+    $scope.addCode = function (key) {
+      input.value = input.value + key;
+    };
+
+    $scope.emptyCode = function () {
+      input.value = "";
+    };
+    $scope.emptyCode();
+  }
 });
