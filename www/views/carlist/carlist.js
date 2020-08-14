3220cca644b4ac5cf145952b9e08ace10802a8c9
EnkhturBaina <eegii.111.gg@gmail.com>
2020-08-14 10:44:19 +0800

    carlist fixed

--- www/views/carlist/carlist.js
+++ www/views/carlist/carlist.js
@@ -1,17 +1,27 @@
 angular.module("carlist.Ctrl", []).controller("carlistCtrl", function ($ionicSlideBoxDelegate, $scope) {
   $scope.grid = function () {
-    document.getElementById("glayout").setAttribute("class", "grid");
-    var arr = Array.prototype.slice.call(document.getElementsByClassName("car-detail-column"));
-    arr.forEach((element) => {
-      element.setAttribute("class", "car-detail-grid");
-    });
-    // document.getElementById("layout-icon").setAttribute("class", "ion-grid");
-  };
-  $scope.column = function () {
-    document.getElementById("glayout").setAttribute("class", "");
-    var arr = Array.prototype.slice.call(document.getElementsByClassName("car-detail-grid"));
-    arr.forEach((element) => {
-      element.setAttribute("class", "car-detail-column");
-    });
+    if ($("#changeLayout").hasClass("layout-list")) {
+      $("#changeLayout").removeClass("layout-list");
+      $("#changeLayout").addClass("layout-grid");
+
+      $("#buttonIcon").removeClass("ion-grid");
+      $("#buttonIcon").addClass("ion-navicon");
+      document.getElementById("glayout").setAttribute("class", "grid");
+      var arr = Array.prototype.slice.call(document.getElementsByClassName("car-detail-column"));
+      arr.forEach((element) => {
+        element.setAttribute("class", "car-detail-grid");
+      });
+    } else {
+      $("#changeLayout").removeClass("layout-grid");
+      $("#changeLayout").addClass("layout-list");
+
+      $("#buttonIcon").removeClass("ion-navicon");
+      $("#buttonIcon").addClass("ion-grid");
+      document.getElementById("glayout").setAttribute("class", "");
+      var arr = Array.prototype.slice.call(document.getElementsByClassName("car-detail-grid"));
+      arr.forEach((element) => {
+        element.setAttribute("class", "car-detail-column");
+      });
+    }
   };
 });
