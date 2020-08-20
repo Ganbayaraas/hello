4a85f1cab32746f0d3251a475e755717ec56ea30
EnkhturBaina <eegii.111.gg@gmail.com>
2020-08-20 11:52:58 +0800

    logo, default tab

--- www/views/home/home.js
+++ www/views/home/home.js
@@ -1,3 +1,7 @@
-﻿angular.module("home.Ctrl", []).controller("homeCtrl", function ($scope, $ionicPlatform) {
+﻿angular.module("home.Ctrl", []).controller("homeCtrl", function ($scope, $ionicPlatform, $ionicTabsDelegate, $timeout) {
   console.log("request list");
+  $timeout(function () {
+    //Default tab select
+    $ionicTabsDelegate.select(1, false);
+  });
 });
